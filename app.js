f0a84fbd5b3cf1cc19f009708cfb6ac0fa950702
RijlanM10 <rijlan01@gmail.com>
2021-02-01 09:50:19 +0700

    View Engines

--- app.js
+++ app.js
@@ -3,25 +3,32 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for request
 app.listen(3000);
 
 app.get('/', (req, res) => {    
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        { title: 'Im all bite no bark', snippet: 'lorem ipsum dolor sit amet consectetur' },
+        { title: 'Im caughting in your riptide', snippet: 'lorem ipsum dolor sit amet consectetur' },
+        { title: 'I love it when you call me absurd', snippet: 'lorem ipsum dolor sit amet consectetur' }
+    ];
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {    
     // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' })
 });
 
-// redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog' });
 });
 
 // 404
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404' });
 });
