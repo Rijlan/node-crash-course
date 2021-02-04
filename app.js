3674e26dcb128a8f667905e4cb8edb2cc0390d9a
RijlanM10 <rijlan01@gmail.com>
2021-02-04 08:28:59 +0700

    Express Route & MVC

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-const Blog = require('./models/blog');
+
+const blogRoutes = require('./routes/blogRoute');
 
 // express app
 const app = express();
@@ -36,53 +37,7 @@ app.get('/about', (req, res) => {
 });
 
 // blogs route
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then(result => {
-            res.render('index', { title: 'All Blogs', blogs: result });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then(result => {
-            res.redirect('/blogs');
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new Blog' });
-});
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', { title: result.title, blog: result });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/blogs' });
-        })
-        .catch(err => {
-            console.log(err)
-        });
-});
+app.use('/blogs', blogRoutes);
 
 // 404
 app.use((req, res) => {
