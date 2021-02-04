44ab41d91151d705a4176d7e285d901d22457287
RijlanM10 <rijlan01@gmail.com>
2021-02-04 08:38:20 +0700

    Wrap Up

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -17,7 +17,7 @@ const blog_details = (req, res) => {
             res.render('blogs/details', { title: result.title, blog: result });
         })
         .catch(err => {
-            console.log(err);
+            res.status(404).render('404', { title: 'Blog not found' })
         });
 };
 
